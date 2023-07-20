42b0e1e3e7cc610a3f266f18f21d9b8c9eb8cdf6
pankaj7073 <pankajvaishnav128@gmail.com>
2023-07-20 18:24:53 +0530

    add mongodb atalas

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cookiParser = require('cookie-parser');
-// error.js 
-const errorMiddleware = require('./middleware/error');
+const cookiParser = require("cookie-parser");
+// error.js
+const errorMiddleware = require("./middleware/error");
 
 app.use(express.json());
 app.use(cookiParser());
@@ -12,13 +12,12 @@ const product = require("./routes/productRoute");
 const user = require("./routes/userRoute");
 const order = require("./routes/orderRoute");
 
-app.use("/api/v1",product);
-app.use("/api/v1/",user);
-app.use("/api/v1",order)
+app.use("/api/v1", product);
+app.use("/api/v1/", user);
+app.use("/api/v1", order);
 
 // middleware for Errors
 app.use(errorMiddleware);
-
 
 module.exports = app;
 
--- backend/config/database.js
+++ backend/config/database.js
@@ -1,9 +1,14 @@
 const mongoose = require("mongoose");
 
 const connectDatabase = () => {
-  mongoose.connect(process.env.DB_URI).then((data) => {
-    console.log(`database is connected ${data.connection.host}`);
-  });
+  mongoose
+    .connect(process.env.DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log(`database is connected`);
+    });
 };
 
 module.exports = connectDatabase;
