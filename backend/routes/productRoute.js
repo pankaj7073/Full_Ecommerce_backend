5fecd1a70537c66ee44f3e2d53208c8d143fe1f2
pankaj7073 <pankajvaishnav128@gmail.com>
2023-07-21 12:34:34 +0530

    home route set

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -13,7 +13,7 @@ const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
 const router = express.Router();
 
-router.route("/products").get(getAllProducts);
+router.route("/").get(getAllProducts);
 
 router
   .route("/admin/product/new")
@@ -23,10 +23,10 @@ router
   .route("/admin/product/:id")
   .put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct)
   .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct);
-  
+
 router.route("/product/:id").get(getProductDetails);
 router.route("/review").put(isAuthenticatedUser, createProductReview);
 
 router.route("/reviews").get(getProductReviews).delete(deleteReview);
-  
+
 module.exports = router;
